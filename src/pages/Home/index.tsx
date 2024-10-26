4c43170375c50ce5a9eab03b0f9b93226ce3f11f
LuisHenriqueXavier <luishenriquexavier@outloo.com>
2024-10-26 03:52:02 -0300

    Feat: finalizado estilização da home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
 import { Play } from "phosphor-react";
-import { CountDownContainer, FormContainer, HomeContainer, Separator } from "./styles";
+import { CountDownContainer, FormContainer, HomeContainer, MinuesAmountInput, Separator, StartCountdownButton, TaskInput } from "./styles";
 
 export function Home() {
     return (
@@ -7,10 +7,18 @@ export function Home() {
             <form action="">
                 <FormContainer>
                 <label htmlFor="task">Vou trabalhar em</label>
-                <input type="text" id="task" />
+                <TaskInput  
+                    type="text" 
+                    id="task" 
+                    placeholder="Dê um nome para o seu projeto" 
+                />
 
                 <label htmlFor="minutesAmount">durante</label>
-                <input type="number" id="minutesAmount" />
+                <MinuesAmountInput 
+                    type="number" 
+                    id="minutesAmount" 
+                    placeholder="00"
+                />
 
                 <span>minutos.</span>
                 </FormContainer>
@@ -23,10 +31,10 @@ export function Home() {
                     <span> 0 </span>
                 </CountDownContainer>
 
-                <button type="submit">
+                <StartCountdownButton disabled type="submit">
                     <Play />
                     Começar
-                </button>
+                </StartCountdownButton>
             </form>
         </HomeContainer>
     )

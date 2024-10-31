0ba8c0873055253a312ea059b56f7355f777d3b8
LuisHenriqueXavier <luishenriquexavier@outloo.com>
2024-10-31 05:44:38 -0300

    Feat: criada as bolinhas de status da tarefa

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,9 +1,9 @@
-import { HistoryContainer, HistoryList } from "./styles"
+import { HistoryContainer, HistoryList, Status } from "./styles"
 
 export function History() {
     return (
         <HistoryContainer>
-            <h1>History</h1>
+            <h1>Meu Histórico</h1>
             <HistoryList>
                 <table>
                     <thead>
@@ -19,25 +19,33 @@ export function History() {
                             <td>Tarefa</td>
                             <td>20 minutos</td>
                             <td>Há 2 meses</td>
-                            <td>Concluídos</td>
+                            <td>
+                                <Status statusColor="green">Concluído</Status>
+                            </td>
                         </tr>
                         <tr>
                             <td>Tarefa</td>
                             <td>20 minutos</td>
                             <td>Há 2 meses</td>
-                            <td>Concluídos</td>
+                            <td>
+                            <Status statusColor="green">Concluído</Status>
+                            </td>
                         </tr>
                         <tr>
                             <td>Tarefa</td>
                             <td>20 minutos</td>
                             <td>Há 2 meses</td>
-                            <td>Concluídos</td>
+                            <td>
+                            <Status statusColor="green">Concluído</Status>
+                            </td>
                         </tr>
                         <tr>
                             <td>Tarefa</td>
                             <td>20 minutos</td>
                             <td>Há 2 meses</td>
-                            <td>Concluídos</td>
+                            <td>
+                            <Status statusColor="green">Concluído</Status>
+                            </td>
                         </tr>
                     </tbody>
                 </table>

12e28c6c1962e527fe949de078459708f6810e1f
LuisHenriqueXavier <luishenriquexavier@outloo.com>
2024-11-14 15:33:17 -0300

    Feat: iniciando novos ciclos na aplicação, começando a adicionar funcionalidade na aplicação

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,6 +2,7 @@ import { Play } from "phosphor-react";
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as zod from 'zod'
+import { useState } from "react";
 
 import { CountDownContainer,
     FormContainer, 
@@ -22,23 +23,44 @@ const newCycleFormValidationSchema = zod.object({
     .max(60, ' Ociclo precisa ser de no máximo de 60 minutos'),
 })
 
-
-
-export function Home() {
-    const { register, handleSubmit, watch} = useForm<NewCycleFormData>({
-        resolver: zodResolver(newCycleFormValidationSchema),
-        defaultValues: {
-            task:'',
-            minutesAmount: 0,
-        }
-    })
-
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
+interface Cycle {
+    id: string
+    task: string
+    minutesAmount: number
+}
+
+export function Home() {
+    const [cycles, setCycles] = useState<Cycle[]>([])
+    const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+
+    const { register, handleSubmit, watch, reset} = useForm<NewCycleFormData>({
+        resolver: zodResolver(newCycleFormValidationSchema),
+        defaultValues: {
+            task: '',
+            minutesAmount: 0,
+        },
+    })
+
     function handleCreateNewCycle(data: NewCycleFormData) {
-        console.log(data)
+        const id = String(new Date().getTime());
+
+        const newCycle: Cycle = {
+            id,
+            task: data.task,
+            minutesAmount: data.minutesAmount,
+        }
+
+        setCycles(state => [...state, newCycle])
+        setActiveCycleId(id)
+
+        reset()
     }
 
+    const activeCycle = cycles.find(cycle => cycle.id === activeCycleId)
+
+    console.log(activeCycle)
 
     const task = watch('task')
     const isSubmitDisabled = !task
